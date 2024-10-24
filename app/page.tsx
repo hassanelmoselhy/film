4ba3e039116c6a36d92438c1e28bea1fbb55ec79
Kareem <kareemadel10110@gmail.com>
2024-10-24 13:42:51 +0300

    theme toggle

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
-import { ThemeSwitcher } from "@/components/ThemeSwitcher";
+import { ModeToggle } from '@/components/ModeToggle';
+
 export default function Home() {
   return (
     <main>
       <div className="text-2xl text-center">Hello world!</div>
-      <ThemeSwitcher />
+      <section className='flex w-full justify-between bg-red-90 px-[20%]'>
+        <ModeToggle />
+
+      </section>
     </main>
   );
 }
